57a1b9d2257298c5378575d346eea0dcc219b9c7
heb229 <heb229@nau.edu>
2026-01-29 10:52:05 -0700

    Update script.js

--- mysite/script.js
+++ mysite/script.js
@@ -3,11 +3,15 @@ const search = document.getElementById('search')
 const genre = document.getElementById('genre')
 const sort = document.getElementById('sort')
 
+// default order
+let sortOrder = 'asc'
+
 async function loadMovies() {
   const params = new URLSearchParams({
     search: search.value,
     genre: genre.value,
-    sort: sort.value
+    sort: sort.value,
+    order: sortOrder
   })
 
   const res = await fetch(`/api/movies?${params}`)
@@ -15,21 +19,32 @@ async function loadMovies() {
 
   list.innerHTML = ''
 
+  if (movies.length === 0) {
+    list.innerHTML = '<li>No movies found</li>'
+    return
+  }
+
   movies.forEach(movie => {
     const li = document.createElement('li')
     li.innerHTML = `
       <a href="movie.html?id=${movie.id}">
-        ${movie.title} (${movie.year}) – Rating: ${movie.rating}
+        ${movie.title} (${movie.year}) – Rating: ${movie.rating} – <em>${movie.genre}</em>
       </a>
     `
     list.appendChild(li)
   })
 }
 
+// Event listeners
 search.oninput = loadMovies
 genre.onchange = loadMovies
 sort.onchange = loadMovies
 
+// Double-click sort dropdown to toggle ascending/descending
+sort.ondblclick = () => {
+  sortOrder = sortOrder === 'asc' ? 'desc' : 'asc'
+  loadMovies()
+}
+
+// Initial load
 loadMovies()
-
-
