f1c6d42ce575ed2f55dce2232b1ae5486a6aa1c1
heb229 <heb229@nau.edu>
2026-01-29 22:01:37 -0700

    Update person.js

--- mysite/api/person.js
+++ mysite/api/person.js
@@ -1,50 +1,45 @@
 /*
 Page for the actors/directors
 */
+ 
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient
-  (
+const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-  )
+  process.env.SUPABASE_ANON_KEY,
+);
 
-
-export default async function handler(req, res) 
-  {
-  const { id } = req.query
+export default async function people_handler(req, res) {
+  const { id } = req.query;
 
   // get person
   const { data: person, error: personError } = await supabase
-    .from('people')
-    .select('*')
-    .eq('id', id)
-    .single()
+    .from("people")
+    .select("*")
+    .eq("id", id)
+    .single();
 
-  if (personError) 
-    {
-    return res.status(500).json(
-      { 
-        error: personError.message 
-      })
-    }
+  if (personError) {
+    return res.status(500).json({
+      error: personError.message,
+    });
+  }
 
   // movies they acted in
   const { data: actedMovies = [] } = await supabase
-    .from('movie_cast')
-    .select('movies(*)')
-    .eq('person_id', id)
+    .from("movie_cast")
+    .select("movies(*)")
+    .eq("person_id", id);
 
   // movies they directed
   const { data: directedMovies = [] } = await supabase
-    .from('movies')
-    .select('*')
-    .eq('director_id', id)
+    .from("movies")
+    .select("*")
+    .eq("director_id", id);
 
   res.status(200).json({
     ...person,
-    actedMovies: actedMovies.map(r => r.movies),
-    directedMovies
-  })
+    actedMovies: actedMovies.map((r) => r.movies),
+    directedMovies,
+  });
 }
