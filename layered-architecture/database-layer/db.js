460a5ab70c8945272af31428dedb4f5a0beeef64
Latisha Talayumptewa <lt537@nau.edu>
2026-03-22 12:27:11 -0700

    added gitignore

--- layered-architecture/database-layer/db.js
+++ layered-architecture/database-layer/db.js
@@ -1,15 +1,18 @@
 import pkg from 'pg'
 const { Pool } = pkg;
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "movie_review_db",
-    password: "8097531*rH12",
-    port: 5432
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "moviedb",
 });
 
-pool.connect()
+pool.query('SELECT 1')
     .then(() => console.log("Connected to PostgreSQL database."))
     .catch((err) => console.error("Database connection error", err.message));
 
